702983ae2608fc679fa0881d22aee75be1ddd415
Jonas Hinterdorfer <j.hinterdorfer@HTBLALeonding.onmicrosoft.com>
2025-12-03 19:25:46 +0100

    Refactor Book class to consolidate regex validation for PublishedDate and improve property assignments

--- Backend/WebAPI/Book.cs
+++ Backend/WebAPI/Book.cs
@@ -5,31 +5,33 @@ using System.Text.RegularExpressions;
 
 public class Book
 {
-    public int Id { get; set; }
-    
+    private static readonly Regex Regex = new Regex(@"^(?:\d{4})-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12]\d|3[01])$");
+    public int   Id { get; set; }
+
     public string Title { get; set; }
-    
+
     public string Author { get; set; }
 
     private string publishedDate = String.Empty;
+
     public string PublishedDate
     {
         get => publishedDate;
         set
         {
-           var regex = new Regex(@"^(?:\d{4})-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12]\d|3[01])$");
-           if (regex.Match(value).Success)
-           {
-               publishedDate = value;
-           }
-           else
-           {
-               throw new ArgumentException("Invalid date");
-           }
+            if (Regex.Match(value).Success)
+            {
+                publishedDate = value;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid date");
+            }
         }
     }
 
     private decimal _price = 0;
+
     public decimal Price
     {
         get => _price;
@@ -39,6 +41,7 @@ public class Book
             {
                 throw new ArgumentException("Price cannot be negative");
             }
+
             _price = value;
         }
     }
@@ -47,16 +50,15 @@ public class Book
 
     public Book()
     {
-        
     }
+
     public Book(int id, BookCreateDto book) : this()
     {
-        this.Id = id;
-        this.Title = book.Title;
-        this.Author = book.Author;
+        this.Id            = id;
+        this.Title         = book.Title;
+        this.Author        = book.Author;
         this.PublishedDate = book.PublishedDate;
-        this.Price = book.Price;
-        this.IsAvailable = book.IsAvailable;
+        this.Price         = book.Price;
+        this.IsAvailable   = book.IsAvailable;
     }
 }
-
